a386a64d1ae2a2ab6709bcfbd2f0b16b00a5cb3e
Chetachi <vichetachi@gmail.com>
2021-09-01 17:05:47 +0100

    debuged

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
+import './App.css'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         Learn React
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
