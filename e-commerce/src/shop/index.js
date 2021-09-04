de03589f4cef3fc8d20038f085cc7681724cbc82
Chetachi <vichetachi@gmail.com>
2021-09-04 21:26:14 +0100

    created and updated index section

---
+++ e-commerce/src/shop/index.js
@@ -0,0 +1,26 @@
+const SHOP_DATA = [
+    {
+        id: 1,
+        title: 'Studio Bag',
+        description: 'The Studio Bag is identical to its predecessor plus a few more iches to '
+        imageUrl: 'https://i.ibb.co/PcXVJ8m/studiobag.png',
+        price: 15,
+    },
+    {
+        id: 2,
+        title: 'Cumulus',
+        description: 'Light as air, the Cumulus in the cloud waterproof nylon and it'
+    },
+     {
+        id: 3,
+        title: 'Studio Bag',
+        description: 'The Studio Bag is identical to its predecessor plus a few more iches to '
+        imageUrl: 'https://i.ibb.co/PcXVJ8m/studiobag.png',
+        price: 15,
+    },
+    {
+        id: 4,
+        title: 'Cumulus',
+        description: 'Light as air, the Cumulus in the cloud waterproof nylon and it'
+    },
+]
